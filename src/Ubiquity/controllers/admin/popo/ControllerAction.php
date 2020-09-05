58b2746d621721b8cd9d886833d120eb04ced1d4
jcheron <myaddressmail@gmail.com>
2020-09-05 03:10:56 +0200

    add onError in excluded actions list

--- src/Ubiquity/controllers/admin/popo/ControllerAction.php
+++ src/Ubiquity/controllers/admin/popo/ControllerAction.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Ubiquity\controllers\admin\popo;
 
 use Ubiquity\cache\CacheManager;
@@ -11,13 +10,36 @@ use Ubiquity\controllers\seo\SeoController;
 use Ubiquity\utils\base\UString;
 
 class ControllerAction {
+
 	private $controller;
+
 	private $action;
+
 	private $parameters;
+
 	private $dValues;
+
 	private $annots;
-	private static $excludeds = [ "__construct","isValid","initialize","finalize","onInvalidControl","loadView","forward","redirectToRoute","getView","message","loadDefaultView","getDefaultViewName","showSimpleMessage","showConfMessage" ];
-	public static $controllers = [ ];
+
+	private static $excludeds = [
+		"__construct",
+		"isValid",
+		"initialize",
+		"finalize",
+		"onInvalidControl",
+		"onError",
+		"loadView",
+		"forward",
+		"redirectToRoute",
+		"getView",
+		"message",
+		"loadDefaultView",
+		"getDefaultViewName",
+		"showSimpleMessage",
+		"showConfMessage"
+	];
+
+	public static $controllers = [];
 
 	public function __construct($controller = "", $action = "", $parameters = [], $dValues = [], $annots = []) {
 		$this->controller = $controller;
@@ -28,63 +50,63 @@ class ControllerAction {
 	}
 
 	public static function initWithPath($url) {
-		$result = [ ];
-		$config = Startup::getConfig ();
-		$ns = $config ["mvcNS"] ["controllers"];
+		$result = [];
+		$config = Startup::getConfig();
+		$ns = $config["mvcNS"]["controllers"];
 		if ($ns !== "" && $ns !== null) {
 			$ns .= "\\";
 		}
 		if (! $url) {
 			$url = "_default";
 		}
-		if (UString::endswith ( $url, "/" ))
-			$url = \substr ( $url, 0, strlen ( $url ) - 1 );
-		$u = \explode ( "/", $url );
-		$u [0] = $ns . $u [0];
-		if (\class_exists ( $u [0] )) {
-			$controllerClass = $u [0];
-			if (\count ( $u ) < 2)
-				$u [] = "index";
-			if (\method_exists ( $controllerClass, $u [1] )) {
-				$method = new \ReflectionMethod ( $u [0], $u [1] );
-				$r = self::scanMethod ( $controllerClass, $method );
-				if (isset ( $r ))
-					$result [] = $r;
+		if (UString::endswith($url, "/"))
+			$url = \substr($url, 0, strlen($url) - 1);
+		$u = \explode("/", $url);
+		$u[0] = $ns . $u[0];
+		if (\class_exists($u[0])) {
+			$controllerClass = $u[0];
+			if (\count($u) < 2)
+				$u[] = "index";
+			if (\method_exists($controllerClass, $u[1])) {
+				$method = new \ReflectionMethod($u[0], $u[1]);
+				$r = self::scanMethod($controllerClass, $method);
+				if (isset($r))
+					$result[] = $r;
 			}
 		}
 		return $result;
 	}
 
 	public static function init() {
-		$result = [ ];
-		$config = Startup::getConfig ();
+		$result = [];
+		$config = Startup::getConfig();
 
-		$files = CacheManager::getControllersFiles ( $config, true );
+		$files = CacheManager::getControllersFiles($config, true);
 		try {
-			$restCtrls = CacheManager::getRestCache ();
-		} catch ( \Exception $e ) {
-			$restCtrls = [ ];
+			$restCtrls = CacheManager::getRestCache();
+		} catch (\Exception $e) {
+			$restCtrls = [];
 		}
 
-		foreach ( $files as $file ) {
-			if (is_file ( $file )) {
-				$controllerClass = ClassUtils::getClassFullNameFromFile ( $file );
-				self::initFromClassname ( $result, $controllerClass, $restCtrls );
+		foreach ($files as $file) {
+			if (is_file($file)) {
+				$controllerClass = ClassUtils::getClassFullNameFromFile($file);
+				self::initFromClassname($result, $controllerClass, $restCtrls);
 			}
 		}
 		return $result;
 	}
 
 	public static function initFromClassname(&$result, $controllerClass, $restCtrls = []) {
-		if (class_exists ( $controllerClass, true ) && isset ( $restCtrls [$controllerClass] ) === false) {
-			self::$controllers [] = $controllerClass;
-			$reflect = new \ReflectionClass ( $controllerClass );
-			if (! $reflect->isAbstract () && $reflect->isSubclassOf ( Controller::class ) && ! $reflect->isSubclassOf ( SeoController::class )) {
-				$methods = $reflect->getMethods ( \ReflectionMethod::IS_PUBLIC );
-				foreach ( $methods as $method ) {
-					$r = self::scanMethod ( $controllerClass, $method );
-					if (isset ( $r ))
-						$result [] = $r;
+		if (class_exists($controllerClass, true) && isset($restCtrls[$controllerClass]) === false) {
+			self::$controllers[] = $controllerClass;
+			$reflect = new \ReflectionClass($controllerClass);
+			if (! $reflect->isAbstract() && $reflect->isSubclassOf(Controller::class) && ! $reflect->isSubclassOf(SeoController::class)) {
+				$methods = $reflect->getMethods(\ReflectionMethod::IS_PUBLIC);
+				foreach ($methods as $method) {
+					$r = self::scanMethod($controllerClass, $method);
+					if (isset($r))
+						$result[] = $r;
 				}
 			}
 		}
@@ -92,16 +114,16 @@ class ControllerAction {
 
 	private static function scanMethod($controllerClass, \ReflectionMethod $method) {
 		$result = null;
-		if (\array_search ( $method->name, self::$excludeds ) === false && ! UString::startswith ( $method->name, "_" )) {
-			$annots = Router::getAnnotations ( $controllerClass, $method->name );
-			$parameters = $method->getParameters ();
-			$defaults = [ ];
-			foreach ( $parameters as $param ) {
-				if ($param->isOptional () && ! $param->isVariadic ()) {
-					$defaults [$param->name] = $param->getDefaultValue ();
+		if (\array_search($method->name, self::$excludeds) === false && ! UString::startswith($method->name, "_")) {
+			$annots = Router::getAnnotations($controllerClass, $method->name);
+			$parameters = $method->getParameters();
+			$defaults = [];
+			foreach ($parameters as $param) {
+				if ($param->isOptional() && ! $param->isVariadic()) {
+					$defaults[$param->name] = $param->getDefaultValue();
 				}
 			}
-			$result = new ControllerAction ( $controllerClass, $method->name, $parameters, $defaults, $annots );
+			$result = new ControllerAction($controllerClass, $method->name, $parameters, $defaults, $annots);
 		}
 		return $result;
 	}
@@ -152,11 +174,11 @@ class ControllerAction {
 	}
 
 	public function getPath() {
-		$reflect = new \ReflectionClass ( $this->controller );
-		return $reflect->getShortName () . "/" . $this->action;
+		$reflect = new \ReflectionClass($this->controller);
+		return $reflect->getShortName() . "/" . $this->action;
 	}
 
 	public function getId() {
-		return $this->getPath ();
+		return $this->getPath();
 	}
 }
