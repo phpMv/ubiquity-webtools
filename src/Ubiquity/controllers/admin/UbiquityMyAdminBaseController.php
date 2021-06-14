a4dd0edbd1a2bdad5de5bed2e16e0ef628d59644
jcheron <myaddressmail@gmail.com>
2021-06-15 01:46:43 +0200

    Update version to 2.4.9

--- src/Ubiquity/controllers/admin/UbiquityMyAdminBaseController.php
+++ src/Ubiquity/controllers/admin/UbiquityMyAdminBaseController.php
@@ -140,7 +140,7 @@ class UbiquityMyAdminBaseController extends Controller implements HasModelViewer
 		]
 	];
 
-	public const version = '2.4.8+';
+	public const version = '2.4.9';
 
 	public $style;
 
