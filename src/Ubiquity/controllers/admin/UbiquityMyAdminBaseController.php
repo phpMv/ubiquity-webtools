58f4ccf33bcb694457597b988c97333963b438ff
J-C HERON <myaddressmail@gmail.com>
2022-04-25 00:59:08 +0200

    Update UbiquityMyAdminBaseController.php

--- src/Ubiquity/controllers/admin/UbiquityMyAdminBaseController.php
+++ src/Ubiquity/controllers/admin/UbiquityMyAdminBaseController.php
@@ -145,7 +145,7 @@ class UbiquityMyAdminBaseController extends Controller implements HasModelViewer
 		]
 	];
 
-	public const version = '2.4.15';
+	public const version = '2.4.16';
 
 	public $style;
 
